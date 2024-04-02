a87e1d081d3e0cd800e05a15d15f52cc47432fff
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-04-02 23:28:17 +0200

    add deploy

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='/camper'>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <App />
         </BrowserRouter>
       </PersistGate>
